d1cff667b4483ea5b1eaa58f255158252993d9fd
muntashir-wahid <muntashir.wahid@yahoo.com>
2023-01-19 20:02:53 +0600

    created footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
-      <h5>Hello from the footer</h5>
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>Copyright © 2023 - All right reserved by Muntashir Wahid</p>
+      </div>
     </footer>
   );
 };
